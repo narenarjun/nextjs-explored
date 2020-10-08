73ebe706b11f9c0f1f6d063eb2649e636b8eef52
Narendran <narendev0610@gmail.com>
2020-10-08 11:57:25 +0530

    all about routing and navigating done

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
 import React from "react";
+import Link from "next/link";
 
-const Page = () => <div>Index Page </div>;
 
+// ! this is how navigation to static path workks
+const Page = () => (
+  <div>
+    <h1>Index Page</h1>
+    <Link href="/notes">
+      <a>Link</a>
+    </Link>
+  </div>
+);
 /**
  * # some cavets:
  * * pages folder can be in the root or inside src folder\
- * * if both pages folder exists[one in the root, one inside src/ folder]  the folder in the root takes precedence. 
+ * * if both pages folder exists[one in the root, one inside src/ folder]  the folder in the root takes precedence.
  */
 
 export default Page;
